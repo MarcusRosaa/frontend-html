710badaf314b2e13d5835a1ee3aca1d71e530482
Marcus Rosa <marcusvrosa1@gmail.com>
2021-03-09 13:30:44 -0300

    adicionando responsividade

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,7 +1,8 @@
 $('.header-span').click(function () { 
     if ($('#dropdown-menu').first().is(":hidden") ){
-        $('#dropdown-menu').css({'padding-top': '10px'})
-        $('#dropdown-menu').slideDown("slow");
+        $('#dropdown-menu').css({'padding-top': '10px'});
+
+        $('#dropdown-menu').slideToggle();
 
     }else{
         $('#dropdown-menu').hide();
